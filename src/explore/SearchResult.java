33e336af45af8bd40319a92bee13a386e3826877
Aladin <Matteocannata19@gmail.com>
2024-03-26 22:47:03 +0100

    Co-authored-by: Hotz99 <Hotz99@users.noreply.github.com> Co-authored-by: front-depiction <front-depiction@users.noreply.github.com>

---
+++ src/explore/SearchResult.java
@@ -0,0 +1,18 @@
+package explore;
+
+import javax.swing.text.html.ListView;
+
+import user.User;
+
+public class SearchResult {
+
+    private User user;
+    private String contentPath;
+    
+
+
+    public ListView<String> createSearchResultList() {
+
+    }
+    
+}
